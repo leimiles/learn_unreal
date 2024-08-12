0f878aafb3972afd29b8a5d898080b679b08b4d6
zhulei <zhulei@xmfunny.com>
2024-08-12 17:31:19 +0800

    <feature> ref & pointer
    ref used as another name to a variable, &ref is an address to the variable; pointer used as an address, *pointer is the content of the value

--- Source/learn01/Private/MilesTools.cpp
+++ Source/learn01/Private/MilesTools.cpp
@@ -5,6 +5,13 @@
 
 MilesTools::MilesTools()
 {
+    float a = 1.0f;
+    float &aRef = a; // init a ref from a variable
+
+    aRef = 3.0f; // to use ref, becuse we dont' want a copy of the value
+
+    float *floatPtr = &aRef; // assign an address to a pointer
+    *floatPtr = 5.0f;        // use pointer to change the value
 }
 
 MilesTools::~MilesTools()
